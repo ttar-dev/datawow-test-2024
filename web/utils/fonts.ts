9fb22bf4c829ada5f984954a6e409c9dcfe85e1b
K. Nattawut <ta.nattawut@icloud.com>
2024-06-28 16:08:03 +0700

    chore: Update global CSS and layout

--- web/utils/fonts.ts
+++ web/utils/fonts.ts
@@ -1,6 +1,7 @@
 import {Inter as FontSans, Castoro as FontBrand} from "next/font/google";
 
 export const fontBranded = FontBrand({
+    subsets: ["latin"],
     variable: "--font-brand",
     weight: ["400"]
 });
