81391cd9309b1ec07d63bb843981852d728ceb1d
K. Nattawut <ta.nattawut@icloud.com>
2024-06-28 18:47:32 +0700

    refactor: image size

--- web/app/(auth)/signin/index.tsx
+++ web/app/(auth)/signin/index.tsx
@@ -5,7 +5,7 @@ export default function SignIn() {
     return (
         <div className="flex flex-col-reverse sm:flex-row bg-content1 min-h-screen text-content1-foreground">
             <div className="flex flex-1 justify-center items-center">
-                <div className="w-login space-y-6">
+                <div className="w-login space-y-6 p-4">
                     <h3 className="font-bold text-2.5xl">Sign In</h3>
                     <div className="space-y-4 flex flex-col">
                         <Input
@@ -23,12 +23,11 @@ export default function SignIn() {
                     </div>
                 </div>
             </div>
-            <div className="flex flex-col items-center gap-4 sm:flex-1 bg-primary-300 min-h-[30vh] sm:max-w-[calc(44%)] justify-center sm:rounded-l-4xl rounded-b-4xl text-primary-foreground">
+            <div className="flex flex-col items-center gap-4 sm:flex-1 bg-primary-300 min-h-[50vh] sm:max-w-[calc(44%)] justify-center sm:rounded-l-4xl rounded-b-4xl text-primary-foreground">
                 <Image
                     src="./images/signin-cover.png"
                     alt="Sign In"
-                    width={300}
-                    className="max-h-[230px]"
+                    className="max-h-[132px] sm:max-h-[230px]"
                 />
                 <h3 className="font-brand text-content1-foreground text-2.5xl">
                     a Board
