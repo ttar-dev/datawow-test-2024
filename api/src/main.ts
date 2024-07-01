368e7d3820ecc8df3e13c43d51bc90b85b4bdfa0
K. Nattawut <ta.nattawut@icloud.com>
2024-07-01 14:12:59 +0700

    init api

---
+++ api/src/main.ts
@@ -0,0 +1,29 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('API Document')
+    .setVersion('0.0.0')
+    .addBearerAuth(
+      {
+        name: 'Authorization',
+        description: 'JWT Token',
+        bearerFormat: 'Bearer',
+        scheme: 'Bearer',
+        type: 'http',
+        in: 'Header',
+      },
+      'Access Token',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
